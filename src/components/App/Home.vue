e945f8dfe1a6d64d4063b7d23bfe8cb4d5a1d638
tanchengwei <804589369@qq.com>
2017-12-08 18:12:56 +0800

    file via update

--- src/components/App/Home.vue
+++ src/components/App/Home.vue
@@ -56,13 +56,28 @@
         <p>新睿云为您快速搭建属于自己的私有云环境(VPC)，并提供100%的网络间隔离，确保安全，包括：私有网络、路由器、公网IP和负载均衡器</p>
         <div class="content-carousel">
           <div v-for="(item,index) in cloudContainer">
-            <div style="height:340px;position: relative" v-show="item.select" @mouseenter="item.ME=true"
+            <div style="height:365px;position: relative;text-align: center" v-show="item.select"
+                 @mouseenter="item.ME=true"
                  @mouseleave="item.ME=false">
-              <div :class="{flexCarousel:item.prodItem.length>5,textCenter:item.prodItem.length<6}" :ref="item.title">
+              <!-- 如果item.prodItem个数大于5个，则需要轮播条 -->
+              <!-- 绑定style width属性用于计算轮播图实际宽度，主要解决ie兼容性问题 -->
+              <div :class="{flexCarousel:item.prodItem.length>5,textCenter:item.prodItem.length<6}" :ref="item.title"
+                   :style="{width:item.prodItem.length*242-15+'px'}">
                 <div v-for="(content,index) in item.prodItem" class="item"
-                     :class="{lastItem:index==item.prodItem.length-1}">
+                     :class="{lastItem:index==item.prodItem.length-1,active:content.ME}"
+                     @mouseenter="handleMouseEnter(content)"
+                     @mouseleave="handleMouseLeave(content)">
                   <div class="header">
+                    <i></i>
                     <h2>{{content.title}}</h2>
+                  </div>
+                  <div class="body">
+                    <img v-show="!content.ME" :src="content.img">
+                    <img v-show="content.ME" :src="content.hoverImg">
+                  </div>
+                  <div class="foot">
+                    <div style="border-right: 1px solid #cccccc">了解详情</div>
+                    <div>立即使用</div>
                   </div>
                 </div>
               </div>
@@ -100,6 +115,30 @@
         </div>
       </div>
     </div>
+    <!-- 图形化控制台 -->
+    <div class="console-container">
+      <div class="container">
+        <p>图形化控制台，清晰友好的使用体验</p>
+        <span>可以通过简单清晰的操作，实现创建资源，配置网络，远程访问，系统监控等功能。摆脱流程，专注创造。</span>
+        <div class="console-img">
+          <img src="../../assets/img/home/console.png">
+          <div style="width:463px"></div>
+        </div>
+      </div>
+    </div>
+    <!-- 权威认证 -->
+    <div class="authority-container">
+      <div class="container">
+        <p>图形化控制台，清晰友好的使用体验</p>
+        <span>可以通过简单清晰的操作，实现创建资源，配置网络，远程访问，系统监控等功能。摆脱流程，专注创造。</span>
+        <ul>
+          <li v-for="(auth,index) in authorityContainer">
+            <img :src="auth.img">
+            <span>{{auth.title}}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -114,18 +153,48 @@
           {title: '全冗余架构', desc: '高可用保障', img: require('../../assets/img/home/box-container-4.png')},
           {title: '虚拟私有云', desc: '网络隔离、子网分配', img: require('../../assets/img/home/box-container-5.png')}
         ],
-        /* title--产品名  img--未选中img  clickImg--选中img  select--是否选中 */
+        /* title--产品名  img--未选中img  clickImg--选中img  select--是否选中 ME--是否显示左右切换箭头  prodItem中ME表示单个item是否hover选中 */
         cloudContainer: [
           {
             title: '云计算',
             img: require('../../assets/img/home/cloud-1-1.png'),
             clickImg: require('../../assets/img/home/cloud-1-2.png'),
             prodItem: [
-              {title: '弹性云服务器（ECS）', desc: '通用型、内存优化型、高IO型'},
-              {title: '镜像服务', desc: '公共镜像、功能镜像、自定义镜像'},
-              {title: 'ECS快照', desc: '稳定可靠、安全保障'},
-              {title: '裸金属服务器', desc: '专属物理服务器'},
-              {title: '弹性伸缩', desc: '高可用、可视化、低成本'}
+              {
+                title: '弹性云服务器（ECS）',
+                desc: '通用型、内存优化型、高IO型',
+                img: require('../../assets/img/home/cloud-2-1.png'),
+                hoverImg: require('../../assets/img/home/cloud-2-3.png'),
+                ME: false
+              },
+              {
+                title: '镜像服务',
+                desc: '公共镜像、功能镜像、自定义镜像',
+                img: require('../../assets/img/home/cloud-2-1.png'),
+                hoverImg: require('../../assets/img/home/cloud-2-3.png'),
+                ME: false
+              },
+              {
+                title: 'ECS快照',
+                desc: '稳定可靠、安全保障',
+                img: require('../../assets/img/home/cloud-2-1.png'),
+                hoverImg: require('../../assets/img/home/cloud-2-3.png'),
+                ME: false
+              },
+              {
+                title: '裸金属服务器',
+                desc: '专属物理服务器',
+                img: require('../../assets/img/home/cloud-2-1.png'),
+                hoverImg: require('../../assets/img/home/cloud-2-3.png'),
+                ME: false
+              },
+              {
+                title: '弹性伸缩',
+                desc: '高可用、可视化、低成本',
+                img: require('../../assets/img/home/cloud-2-1.png'),
+                hoverImg: require('../../assets/img/home/cloud-2-3.png'),
+                ME: false
+              }
             ],
             select: true,
             ME: false
@@ -135,12 +204,48 @@
             img: require('../../assets/img/home/cloud-2-1.png'),
             clickImg: require('../../assets/img/home/cloud-2-2.png'),
             prodItem: [
-              {title: '弹虚拟私有云VPC', desc: '网络隔离、分配子网'},
-              {title: '弹性IP', desc: '绑定与解绑IP、扩容'},
-              {title: '负载均衡', desc: '源算法、轮询、最小连接数'},
-              {title: 'NAT网关', desc: 'TCP/HTTP协议、多对一支持'},
-              {title: '虚拟专网VPN', desc: '跨VPC链接'},
-              {title: 'CDN', desc: '节点丰富、安全易用'}
+              {
+                title: '弹虚拟私有云VPC',
+                desc: '网络隔离、分配子网',
+                img: require('../../assets/img/home/cloud-2-1.png'),
+                hoverImg: require('../../assets/img/home/cloud-2-3.png'),
+                ME: false
+              },
+              {
+                title: '弹性IP',
+                desc: '绑定与解绑IP、扩容',
+                img: require('../../assets/img/home/cloud-2-1.png'),
+                hoverImg: require('../../assets/img/home/cloud-2-3.png'),
+                ME: false
+              },
+              {
+                title: '负载均衡',
+                desc: '源算法、轮询、最小连接数',
+                img: require('../../assets/img/home/cloud-2-1.png'),
+                hoverImg: require('../../assets/img/home/cloud-2-3.png'),
+                ME: false
+              },
+              {
+                title: 'NAT网关',
+                desc: 'TCP/HTTP协议、多对一支持',
+                img: require('../../assets/img/home/cloud-2-1.png'),
+                hoverImg: require('../../assets/img/home/cloud-2-3.png'),
+                ME: false
+              },
+              {
+                title: '虚拟专网VPN',
+                desc: '跨VPC链接',
+                img: require('../../assets/img/home/cloud-2-1.png'),
+                hoverImg: require('../../assets/img/home/cloud-2-3.png'),
+                ME: false
+              },
+              {
+                title: 'CDN',
+                desc: '节点丰富、安全易用',
+                img: require('../../assets/img/home/cloud-2-1.png'),
+                hoverImg: require('../../assets/img/home/cloud-2-3.png'),
+                ME: false
+              }
             ],
             select: false,
             ME: false
@@ -150,10 +255,34 @@
             img: require('../../assets/img/home/cloud-3-1.png'),
             clickImg: require('../../assets/img/home/cloud-3-2.png'),
             prodItem: [
-              {title: '云硬盘', desc: '性能型、超高性能型、存储型'},
-              {title: '云硬盘快照', desc: '高可用保障、敏捷易用'},
-              {title: '云硬盘备份', desc: '高可用保障、敏捷易用'},
-              {title: '云硬盘热增容', desc: '高可用保障、敏捷易用'}
+              {
+                title: '云硬盘',
+                desc: '性能型、超高性能型、存储型',
+                img: require('../../assets/img/home/cloud-2-1.png'),
+                hoverImg: require('../../assets/img/home/cloud-2-3.png'),
+                ME: false
+              },
+              {
+                title: '云硬盘快照',
+                desc: '高可用保障、敏捷易用',
+                img: require('../../assets/img/home/cloud-2-1.png'),
+                hoverImg: require('../../assets/img/home/cloud-2-3.png'),
+                ME: false
+              },
+              {
+                title: '云硬盘备份',
+                desc: '高可用保障、敏捷易用',
+                img: require('../../assets/img/home/cloud-2-1.png'),
+                hoverImg: require('../../assets/img/home/cloud-2-3.png'),
+                ME: false
+              },
+              {
+                title: '云硬盘热增容',
+                desc: '高可用保障、敏捷易用',
+                img: require('../../assets/img/home/cloud-2-1.png'),
+                hoverImg: require('../../assets/img/home/cloud-2-3.png'),
+                ME: false
+              }
             ],
             select: false,
             ME: false
@@ -163,8 +292,20 @@
             img: require('../../assets/img/home/cloud-4-1.png'),
             clickImg: require('../../assets/img/home/cloud-4-2.png'),
             prodItem: [
-              {title: '防火墙', desc: '自定义规则、协议、端口'},
-              {title: 'DDOS高防IP', desc: '硬件防护、40G超大流量'}
+              {
+                title: '防火墙',
+                desc: '自定义规则、协议、端口',
+                img: require('../../assets/img/home/cloud-2-1.png'),
+                hoverImg: require('../../assets/img/home/cloud-2-3.png'),
+                ME: false
+              },
+              {
+                title: 'DDOS高防IP',
+                desc: '硬件防护、40G超大流量',
+                img: require('../../assets/img/home/cloud-2-1.png'),
+                hoverImg: require('../../assets/img/home/cloud-2-3.png'),
+                ME: false
+              }
             ],
             select: false,
             ME: false
@@ -174,8 +315,20 @@
             img: require('../../assets/img/home/cloud-5-1.png'),
             clickImg: require('../../assets/img/home/cloud-5-2.png'),
             prodItem: [
-              {title: '云监控', desc: '自定义监控项、多告警推送方式'},
-              {title: '访问控制', desc: '权限管理、精准控制'}
+              {
+                title: '云监控',
+                desc: '自定义监控项、多告警推送方式',
+                img: require('../../assets/img/home/cloud-2-1.png'),
+                hoverImg: require('../../assets/img/home/cloud-2-3.png'),
+                ME: false
+              },
+              {
+                title: '访问控制',
+                desc: '权限管理、精准控制',
+                img: require('../../assets/img/home/cloud-2-1.png'),
+                hoverImg: require('../../assets/img/home/cloud-2-3.png'),
+                ME: false
+              }
             ],
             select: false,
             ME: false
@@ -224,6 +377,14 @@
             clickImg: require('../../assets/img/home/feature-6-2.png'),
             select: false
           }
+        ],
+        /* 权威认证 */
+        authorityContainer: [
+          {title: '中国高新技术企业', img: require('../../assets/img/home/authority-1.png')},
+          {title: '中关村高新技术企业', img: require('../../assets/img/home/authority-2.png')},
+          {title: 'ISO27001企业认证', img: require('../../assets/img/home/authority-3.png')},
+          {title: 'ISO9001企业认证', img: require('../../assets/img/home/authority-4.png')},
+          {title: '华为云管理网络ISV', img: require('../../assets/img/home/authority-5.png')}
         ]
       }
     },
@@ -249,17 +410,26 @@
         var clientWidth = this.$refs[title][0].clientWidth
         var offsetLeft = this.$refs[title][0].offsetLeft
         if (direction === 'right') {
-          offsetLeft += 243
+          // 向右滑动  offsetLeft加243px  当offsetLeft大于0 offsetLeft重置0
+          offsetLeft += 245
           if (offsetLeft > 0) {
             offsetLeft = 0
           }
         } else {
-          offsetLeft -= 243
-          if (offsetLeft + clientWidth < 1200) {
-            offsetLeft = 1200 - clientWidth
+          // 向左滑动  offsetLeft减243px  当offsetLeft加上clientWidth还小于1200 offsetLeft重置1200-clientWidth
+          offsetLeft -= 245
+          if (offsetLeft + clientWidth < 1195) {
+            offsetLeft = 1195 - clientWidth
           }
         }
         this.$refs[title][0].style.left = `${offsetLeft}px`
+      },
+      // 捕捉到鼠标移入 具体的item
+      handleMouseEnter(content){
+        content.ME = true
+      },
+      handleMouseLeave(content){
+        content.ME = false
       }
     }
   }
@@ -465,33 +635,99 @@
             transform: translate(16px, -50%);
           }
           .flexCarousel {
-            width: max-content;
-            display: flex;
             position: absolute;
+            display: flex;
+            align-items: center;
+            height: 360px;
             transition: all .5s;
+            left: 0px;
+            margin: 2.5px 2.5px 2.5px;
           }
           .textCenter {
             text-align: center;
+            align-items: center;
+            height: 360px;
+            display: inline-flex;
+            margin: 2.5px auto;
             div:last-child {
               margin-right: 0px;
             }
           }
           .item {
             display: inline-block;
-            width: 228px;
+            width: 227px;
             margin-right: 15px;
-            height: 340px;
             background-color: #ffffff;
             border-radius: 10px;
+            overflow: hidden;
             .header {
               height: 52px;
               border-bottom: 1px solid #999999;
+              position: relative;
               h2 {
                 padding: 18px 0px;
                 text-align: center;
                 font-size: 16px;
                 color: #999999;
                 font-weight: normal;
+              }
+              i {
+                display: block;
+                width: 0px;
+                height: 0px;
+                border-bottom: 20px solid #999999;
+                border-right: 20px solid transparent;
+                border-left: 20px solid transparent;
+                position: absolute;
+                transform: rotate(45deg);
+                right: -14px;
+                top: -3px;
+              }
+            }
+            .body {
+              height: 243px;
+              transition: height .3s;
+              position: relative;
+              img {
+                position: absolute;
+                top: 54px;
+                left: 50%;
+                transform: translateX(-50%);
+              }
+            }
+            .foot {
+              border-top: 1px solid #999999;
+              height: 45px;
+              display: flex;
+              > div {
+                width: 50%;
+                text-align: center;
+                color: #999999;
+                font-size: 14px;
+                vertical-align: middle;
+                line-height: 45px;
+              }
+            }
+          }
+          .active {
+            background-color: #387dff;
+            box-shadow: 0 0px 10px 0 #387dff;
+            .header {
+              border-bottom: 1px solid #ffffff;
+              i {
+                border-bottom: 20px solid #ffe876;
+              }
+              h2 {
+                color: #ffffff;
+              }
+            }
+            .body {
+              height: 263px;
+            }
+            .foot {
+              border-top: 1px solid #ffffff;
+              > div {
+                color: #ffffff;
               }
             }
           }
@@ -549,6 +785,63 @@
           }
           .notMargin {
             margin-bottom: 0px;
+          }
+        }
+      }
+    }
+    > .console-container {
+      background-color: #f9f9f9;
+      .container {
+        padding: 82px 0px 72px;
+        width: 1200px;
+        margin: 0px auto;
+        text-align: center;
+        > p {
+          font-size: 28px;
+          color: #333333;
+          margin-bottom: 25px;
+        }
+        > span {
+          font-size: 14px;
+          color: #999999;
+          margin-bottom: 68px;
+          display: inline-block;
+        }
+        .console-img {
+          display: flex;
+          width: 1160px;
+          margin: 0px auto;
+          background-color: #377dff
+        }
+      }
+    }
+    > .authority-container {
+      padding: 82px 0px 16px;
+      background-color: #ffffff;
+      .container {
+        width: 1200px;
+        margin: 0px auto;
+        text-align: center;
+        > p {
+          font-size: 28px;
+          color: #333333;
+          margin-bottom: 25px;
+        }
+        > span {
+          font-size: 14px;
+          color: #999999;
+          margin-bottom: 50px;
+          display: inline-block;
+        }
+        ul {
+          display: flex;
+          justify-content: space-between;
+          li {
+            font-size: 14px;
+            color: #999999;
+            img {
+              vertical-align: middle;
+            }
           }
         }
       }
