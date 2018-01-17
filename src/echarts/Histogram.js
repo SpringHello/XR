db5e3aa18dd863e144f837c659e763e34c9d75df
lhj <2420479720@qq.com>
2018-01-17 09:52:40 +0800

    eslint m

--- src/echarts/Histogram.js
+++ src/echarts/Histogram.js
@@ -37,7 +37,7 @@ export default {
       axisLabel: {
         formatter: '{value}%'
       },
-      max: 100,
+      max: 100
     }
   ],
   series: [
@@ -48,4 +48,4 @@ export default {
       data: []
     }
   ]
-};
+}
