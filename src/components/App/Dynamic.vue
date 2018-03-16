ccf7da5d62ad8deddc8ec9889a5ede2401a69832
heyu <784242998@qq.com>
2018-03-16 11:51:35 +0800

    hnurheuh

--- src/components/App/Dynamic.vue
+++ src/components/App/Dynamic.vue
@@ -36,7 +36,7 @@
   export  default {
     data () {
       return {
-        selectAnnouncement: '',
+        selectAnnouncement: null,
         announcementArray: [],
         announcement: null
       }
