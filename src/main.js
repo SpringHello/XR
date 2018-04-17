34ef08cfe23914fa9530274c3762e1dce0e876a7
tanchengwei <804589369@qq.com>
2018-04-17 17:58:39 +0800

    file

--- src/main.js
+++ src/main.js
@@ -31,8 +31,8 @@ Vue.config.productionTip = false
 
 //axios.defaults.baseURL = '/ruicloud'
 
-axios.defaults.baseURL = 'http://192.168.3.204:8081/ruicloud'
-axios.defaults.withCredentials = true
+//axios.defaults.baseURL = 'http://192.168.3.204:8081/ruicloud'
+//axios.defaults.withCredentials = true
 
 // axios挂载到Vue原型
 Vue.prototype.$http = axios.create({
