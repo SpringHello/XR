d018d64ba904a39ac901d0679c0a2eb8e4b8badf
eCheo <e1511404018@qq.com>
2018-05-17 18:05:48 +0800

    'deng'

---
+++ src/echarts/objectStroage.js
@@ -0,0 +1,57 @@
+
+var echarts = require('echarts/lib/echarts')
+export default {
+    tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+            type: 'line',
+            lineStyle: {
+                color: '#000000',
+                opacity: 0.5,
+                type: 'dashed'
+            },
+        }
+    },
+    grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+    },
+    toolbox: {
+        feature: {
+            saveAsImage: {}
+        }
+    },
+    xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ['04/28', '04/28', '04/28', '04/28', '04/28', '04/28', '04/28']
+    },
+    yAxis: {
+        axisLabel: {
+            formatter: '{value} %'
+        },
+        max:'100'
+    },
+    series: [
+        {
+            name: '邮件营销',
+            type: 'line',
+            stack: '总量',
+            itemStyle: {
+                normal: {
+                    color: '#2A99F2'
+                }
+            },
+            areaStyle: {normal: {  color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                offset: 0,
+                color: '#2A99F2'
+            }, {
+                offset: 1,
+                color: '#ffe'
+            }])}},
+            data: [20, 25, 35, 45, 55, 65, 75]
+        },
+    ]
+}
---
+++ src/myView/objectStrorage/tabTwo.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+       <div class="center_chart">
+                            <div style="display:flex;border-bottom:1px solid #E9E9E9;padding-bottom:5px;">
+                                <div style="width:50%;font-size:16px;color:#333333;">下载流量情况</div>
+                                <div style="width:50%;text-align:right;color:#666666;">2017.11.25</div>
+                            </div>
+                            <div style="margin-top:10px;">
+                                <div class="chart-text">
+                                    <span >今天</span>
+                                    <span >昨天</span>
+                                    <span >最近7天</span>
+                                    <span >最近30天</span>
+                                </div>
+                                <div style="float:right">
+                                    <Button type="primary" size="small" style="margin-right:30px;margin-top:-3px;">导出</Button>
+                                     <span style="">折线</span>
+                                    <span >柱状图</span>
+                                </div>
+                            </div>
+                            <chart class="echarts" :options="rwPolar"></chart>
+                        </div>
+                         <div class="center_chart">
+                            <div style="display:flex;border-bottom:1px solid #E9E9E9;padding-bottom:5px;margin-top:50px;">
+                                <div style="width:50%;font-size:16px;color:#333333;">Get请求情况</div>
+                                <div style="width:50%;text-align:right;color:#666666;">2017.11.25</div>
+                            </div>
+                            <div style="margin-top:10px;">
+                                <div class="chart-text">
+                                    <span >今天</span>
+                                    <span >昨天</span>
+                                    <span >最近7天</span>
+                                    <span >最近30天</span>
+                                </div>
+                                <div style="float:right">
+                                    <Button type="primary" size="small" style="margin-right:30px;margin-top:-3px;">导出</Button>
+                                     <span style="">折线</span>
+                                    <span >柱状图</span>
+                                </div>
+                            </div>
+                            <chart class="echarts" :options="rwPolar"></chart>
+                        </div>    
+                        <div class="center_chart">
+                            <div style="display:flex;border-bottom:1px solid #E9E9E9;padding-bottom:5px;margin-top:50px;">
+                                <div style="width:50%;font-size:16px;color:#333333;">Put请求情况</div>
+                                <div style="width:50%;text-align:right;color:#666666;">2017.11.25</div>
+                            </div>
+                            <div style="margin-top:10px;">
+                                <div class="chart-text">
+                                    <span >今天</span>
+                                    <span >昨天</span>
+                                    <span >最近7天</span>
+                                    <span >最近30天</span>
+                                </div>
+                                <div style="float:right">
+                                    <Button type="primary" size="small" style="margin-right:30px;margin-top:-3px;">导出</Button>
+                                     <span style="">折线</span>
+                                    <span >柱状图</span>
+                                </div>
+                            </div>
+                            <chart class="echarts" :options="rwPolar"></chart>
+                        </div>      
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
+
