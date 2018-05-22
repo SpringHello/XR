5c4df9f00372b7f07604704c313ee036cdcb6451
tanchengwei <804589369@qq.com>
2018-05-22 15:54:45 +0800

    file

--- src/Back.vue
+++ src/Back.vue
@@ -8,13 +8,13 @@
         <div class="operate">
           <ul>
             <li>
-              <router-link to="overview" :class="{active:pageInfo.path=='overview'}"><span>总览</span></router-link>
+              <router-link to="overview"><span>总览</span></router-link>
             </li>
             <li>
-              <router-link to="work" :class="{active:pageInfo.path=='work'}"><span>工单</span></router-link>
+              <router-link to="work"><span>工单</span></router-link>
             </li>
             <li>
-              <router-link to="renew" :class="{active:pageInfo.path=='renew'}"><span>一键续费</span></router-link>
+              <router-link to="renew"><span>一键续费</span></router-link>
             </li>
 
           </ul>
@@ -23,16 +23,16 @@
               <router-link to="BRecords" :class="{active:pageInfo.path=='BRecords'}"><span>备案</span></router-link>
             </li>-->
             <li>
-              <router-link to="buy" :class="{active:pageInfo.path=='buy'}"><span>创建主机</span></router-link>
+              <router-link to="buy"><span>创建主机</span></router-link>
             </li>
             <li>
-              <router-link to="document" :class="{active:pageInfo.path=='document'}"><span>帮助文档</span></router-link>
+              <router-link to="document"><span>帮助文档</span></router-link>
             </li>
             <li>
-              <router-link to="recharge" :class="{active:pageInfo.path=='recharge'}"><span>充值</span></router-link>
+              <router-link to="recharge"><span>充值</span></router-link>
             </li>
             <li>
-              <Dropdown @on-click="go">
+              <Dropdown>
                 <a href="javascript:void(0)" style="position:relative">
                   {{userInfo.realname}}
                   <sup class="circle-dot" v-if="this.$store.state.Msg>0"></sup>
@@ -63,62 +63,53 @@
         </div>
       </div>
     </header>
-    <div class="sec-header">
-      <div class="wrapper">
-        <div class="zoneList">
-          <Dropdown @on-click="toggleZone">
-            <div style="height:30px;">
-              <div
-                style="display: inline-block;background: #2A99F2;border-radius: 4px;height: 30px;padding: 4px 0px;cursor:pointer">
-                <img src="./assets/img/back/zoneIcon.png" style="vertical-align: middle;margin:0px 10px;"></img><span
-                style="font-size: 14px;color: #FFFFFF;line-height: 21px;vertical-align: middle;margin-right:10px;">{{zone.zonename}}</span>
-              </div>
-            </div>
-            <DropdownMenu slot="list">
-              <DropdownItem :name="zone.zoneid" v-for="(zone,index) in zoneList" :key="index">
-                {{zone.zonename}}
-              </DropdownItem>
-            </DropdownMenu>
-          </Dropdown>
-        </div>
-        <div class="operate" ref="operate">
-          <ul @mouseleave="ML">
-            <li class="sec-nav" v-for="(item,index) in main" :key="index" @mouseenter="ME($event,item.type)"
-                :ref="item.type"
-                :class="{hover:item.type==pageInfo.hoverItem}">
-              <a>{{item.mainName}}</a>
-            </li>
-            <div class="line" :style="lineStyle" ref="line"></div>
-          </ul>
-        </div>
+    <div style="display: flex">
+      <div>
+        <Menu :active-name="activeName" :open-names="openName" @on-select="push" accordion=true>
+          <Submenu name="service">
+            <template slot="title">
+              <Icon type="ios-analytics"></Icon>
+              云服务器
+            </template>
+            <MenuItem name="host">云主机</MenuItem>
+            <MenuItem name="snapshot">云主机快照</MenuItem>
+            <MenuItem name="mirror">快照</MenuItem>
+          </Submenu>
+          <Submenu name="2">
+            <template slot="title">
+              <Icon type="ios-filing"></Icon>
+              云存储
+            </template>
+            <MenuItem name="disk">云硬盘</MenuItem>
+            <MenuItem name="diskBackup">云硬盘备份</MenuItem>
+          </Submenu>
+          <Submenu name="2">
+            <template slot="title">
+              <Icon type="ios-filing"></Icon>
+              云网络
+            </template>
+            <MenuItem name="vpc">虚拟私有云VPC</MenuItem>
+            <MenuItem name="ip">弹性IP</MenuItem>
+            <MenuItem name="balance">负载均衡</MenuItem>
+            <MenuItem name="vpn">虚拟专网VPN</MenuItem>
+          </Submenu>
+          <Submenu name="2">
+            <template slot="title">
+              <Icon type="ios-filing"></Icon>
+              云安全
+            </template>
+            <MenuItem name="firewall">防火墙</MenuItem>
+          </Submenu>
+          <Submenu name="2">
+            <template slot="title">
+              <Icon type="ios-filing"></Icon>
+              回收站
+            </template>
+            <MenuItem name="recycle">回收站</MenuItem>
+          </Submenu>
+        </Menu>
       </div>
-    </div>
-    <div class="thr-header" @mouseenter="handME" @mouseleave="handML" :class="thrShow">
-      <div class="wrapper">
-        <div class="operate" v-for="(parentItem,pIndex) in main" :key="pIndex" v-if="parentItem.subItem"
-             :style="menuStyle(parentItem.type)">
-          <ul :ref="`${parentItem.type}-sub`" :class="{show:parentItem.type==pageInfo.hoverItem}">
-            <li v-for="(subItem,sIndex) in parentItem.subItem" :key="sIndex"
-                @click="push(parentItem.type,subItem.type)" :class="{hover:subItem.type==pageInfo.sType}">
-              <Dropdown v-if="subItem.thrItem" @on-click="pane">
-                <a href="javascript:void(0)">
-                  {{subItem.subName}}
-                </a>
-                <DropdownMenu slot="list">
-                  <DropdownItem v-for="(thrItem,index) in subItem.thrItem" :key="index"
-                                :name="`${thrItem.pane}#${subItem.subName}`">
-                    <a>{{thrItem.thrName}}</a>
-                  </DropdownItem>
-                </DropdownMenu>
-              </Dropdown>
-              <a v-else>
-                {{subItem.subName}}
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div style="clear:right"></div>
-      </div>
+      <router-view/>
     </div>
     <!-- 客服浮动块 -->
     <div class="affix">
@@ -195,7 +186,7 @@
         <Icon type="chevron-up" class="backtop"></Icon>
       </BackTop>
     </div>
-    <router-view/>
+
   </div>
 </template>
 
@@ -209,23 +200,8 @@
     name: 'back',
     data(){
       return {
-        // pageInfo用于存储当前页面信息
-        pageInfo: {
-          // hover选中的item
-          hoverItem: '',
-          // 点击选中的二级item
-          selectItem: '',
-          // 点击选中的三级item
-          sType: '',
-          // 当前路径
-          path: '',
-          // 是否进入二级menu栏
-          enter2Hover: false,
-          // 是否进入三级menu栏
-          enter3Hover: false,
-          // 锁定三级目录
-          static: false
-        },
+        activeName: 'overview',
+        openName: [],
         main: [
           {
             mainName: '云服务器',
@@ -284,6 +260,7 @@
       }
     },
     beforeRouteEnter(to, from, next){
+
       // 获取所有后台需要的基本信息
       // 获取用户信息
       var userInfo = axios.get('user/GetUserInfo.do')
@@ -298,7 +275,9 @@
             vm.$router.push({path: '/ruicloud/login'})
           })
         }
-        next()
+        next(vm => {
+          vm.activeName = to.name
+        })
       })
     },
     created(){
@@ -314,19 +293,6 @@
       this.notice()
     },
     mounted(){
-      // mounted时期根据路径修改选中的menu
-      this.pageInfo.path = this.$router.history.current.name
-      for (var item of this.main) {
-        if (item.subItem) {
-          for (var sItem of item.subItem) {
-            if (sItem.type == this.pageInfo.path) {
-              this.pageInfo.hoverItem = this.pageInfo.selectItem = item.type
-              this.pageInfo.sType = sItem.type
-              this.pageInfo.static = true
-            }
-          }
-        }
-      }
     },
     methods: {
       QME(){
@@ -346,79 +312,6 @@
           }
         })
       },
-      // 进入二级栏
-      ME: debounce(200, function (event, type) {
-        this.pageInfo.enter2Hover = true
-        this.pageInfo.hoverItem = type
-      }),
-
-      // 退出二级栏
-      ML: debounce(200, function () {
-        if (!this.pageInfo.enter3Hover) {
-          // 没有进入三级目录才能重置
-          this.pageInfo.hoverItem = this.pageInfo.selectItem
-        }
-        this.pageInfo.enter2Hover = false
-      }),
-
-      // 进入三级栏
-      handME(){
-        this.pageInfo.enter3Hover = true
-      },
-
-      // 退出三级栏
-      handML: debounce(300, function () {
-        this.pageInfo.enter3Hover = false
-        if (this.pageInfo.selectItem && !this.pageInfo.enter2Hover) {
-          // 三级页面被选中，回退到选中的三级页面
-          this.pageInfo.hoverItem = this.pageInfo.selectItem
-          return
-        }
-        if (!this.pageInfo.enter2Hover) {
-          this.pageInfo.hoverItem = ''
-        }
-      }),
-
-      // 进入三级路由，记录二级路由入口
-      push(pType, sType){
-        this.pageInfo.static = true
-        this.pageInfo.selectItem = pType
-        this.pageInfo.sType = sType
-        this.$router.push(sType)
-      },
-      go(path){
-        if (path == 'exit') {
-          this.exit()
-          return
-        }
-        this.$router.push(path)
-      },
-      pane(pane){
-        var paneStatue = {
-          vpc: 'VPC',
-          vpn: 'remote'
-        }
-        let arr = pane.split('#')
-        if (arr[1] == '虚拟专网VPN') {
-          paneStatue.vpn = arr[0]
-        } else {
-          paneStatue.vpc = arr[0]
-        }
-        this.$store.commit('setPane', paneStatue)
-      },
-
-      menuStyle(type){
-        if (this.$refs[type]) {
-          var clientWidth = this.$refs[`${type}-sub`][0].clientWidth || this.$refs[`${type}-sub`][0].getBoundingClientRect().width
-          var cw = this.$refs[type][0].clientWidth
-          var c = (clientWidth - cw) / 2
-          var liOffset = this.$refs[type][0].offsetLeft
-          var offset = this.$refs.operate.offsetLeft
-          return {
-            left: `${liOffset + offset - c}px`
-          }
-        }
-      },
       toggleZone(zoneId){
         // 切换默认区域
         axios.get('user/setDefaultZone.do', {params: {zoneId: zoneId}}).then(response => {
@@ -434,66 +327,17 @@
           $store.commit('setAuthInfo', {authInfo: null, userInfo: null})
           this.$router.push('/ruicloud/login')
         })
+      },
+      push(name){
+        this.$router.push(name)
       }
     },
     computed: mapState({
-      // show代表是否显示three menu,static代表是否固定three menu
-      thrShow(){
-        return {
-          show: this.pageInfo.hoverItem != '',
-          static: this.pageInfo.static
-        }
-      },
-      // 计算选中条样式
-      lineStyle(){
-        if (this.$refs[this.pageInfo.hoverItem]) {
-          var style = {
-            left: `${this.$refs[this.pageInfo.hoverItem][0].offsetLeft}px`,
-            width: `${this.$refs[this.pageInfo.hoverItem][0].clientWidth}px`
-          }
-          if (!this.$refs.line.clientWidth) {
-            style.transition = 'width .3s'
-          } else {
-            style.transition = 'all .3s'
-          }
-          return style
-        }
-        if (this.$refs.line) {
-          return {
-            left: `${this.$refs.line.offsetLeft}px`,
-            transition: 'width .3s'
-          }
-        }
-      },
       userInfo: state => state.userInfo,
       zone: state => state.zone,
       zoneList: state => state.zoneList
     }),
-    watch: {
-      '$route'(to, from){
-        // 对路由变化作出响应...
-        this.pageInfo.hoverItem = this.pageInfo.selectItem = this.sType = ''
-        this.pageInfo.static = false
-        this.pageInfo.path = to.name
-        for (var item of this.main) {
-          if (item.subItem) {
-            for (var sItem of item.subItem) {
-              if (sItem.type == this.pageInfo.path) {
-                this.pageInfo.hoverItem = this.pageInfo.selectItem = item.type
-                this.pageInfo.sType = sItem.type
-                this.pageInfo.static = true
-              }
-            }
-          }
-        }
-      },
-      '$store.state.zone': {
-        handler: function () {
-          this.notice()
-        },
-        deep: true
-      }
-    }
+    watch: {}
   }
 </script>
 
@@ -597,119 +441,6 @@
               background-color: rgb(0, 193, 222);
               position: absolute;
               bottom: 0px;
-            }
-          }
-        }
-      }
-    }
-    .sec-header {
-      height: 45px;
-      background-color: #ffffff;
-      .wrapper {
-        width: 1200px;
-        height: 100%;
-        margin: 0px auto;
-        /*display: flex;
-        justify-content: space-between;*/
-        .zoneList {
-          display: inline-block;
-          height: 30px;
-          margin: 7.5px 0px;
-          .ivu-dropdown-rel {
-            a {
-              font-size: 14px;
-              line-height: 45px;
-              display: inline-block;
-              color: #333333;
-            }
-          }
-        }
-        .operate {
-          float: right;
-          > ul {
-            position: relative;
-            .sec-nav {
-              display: inline-block;
-              font-size: 14px;
-              margin: 0px 20px;
-              line-height: 45px;
-              padding: 0px 20px;
-              cursor: pointer;
-              &:last-of-type {
-                margin-right: 0px;
-              }
-              &.hover {
-                a {
-                  color: #2A99F2;
-                }
-              }
-
-              a {
-                color: #333333;
-              }
-            }
-            .line {
-              height: 2px;
-              background-color: #377dff;
-              position: absolute;
-              width: 0px;
-              bottom: 0px;
-              transition: all .3s;
-            }
-          }
-
-        }
-      }
-    }
-    .thr-header {
-      border-top: 1px solid #ccc;
-      overflow-y: hidden;
-      transition: all .3s;
-      height: 0px;
-      // position: absolute;
-      width: 100%;
-      background-color: #ffffff;
-      &.show {
-        border-bottom: 1px solid #ccc;
-        height: 45px;
-      }
-      &.static {
-        // position: static;
-        height: 45px;
-      }
-      .wrapper {
-        // position: relative;
-        text-align: center;
-        .operate {
-          position: absolute;
-          > ul {
-            height: 0px;
-            overflow-y: hidden;
-            transition: all .5s;
-            // position: absolute;
-            > li {
-              display: inline-block;
-              font-size: 14px;
-              color: #333333;
-              line-height: 45px;
-              padding: 0px 20px;
-              cursor: pointer;
-              a {
-                color: #333333;
-              }
-              &:hover {
-                a {
-                  color: #2A99F2;
-                }
-              }
-              &.hover {
-                a {
-                  color: #2A99F2;
-                }
-              }
-            }
-            &.show {
-              height: 45px;
             }
           }
         }
