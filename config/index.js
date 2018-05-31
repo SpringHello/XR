f0da97d650ac70a253a22cfa035528c177f9ccd9
tanchengwei <804589369@qq.com>
2018-05-31 09:25:35 +0800

    Merge branch 'master' of github.com:SpringHello/XR
    # Conflicts:
    #	src/components/App/active/active_2.vue
    #	src/main.js

--- config/index.js
+++ config/index.js
@@ -60,7 +60,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'resource',
-    assetsPublicPath: '/ruicloud/',
+    assetsPublicPath: '/ruirados/',
 
     /**
      * Source Maps
