0143b141532e097d7ad5394a97b7eb791a1b5baf
lhj <2420479720@qq.com>
2018-06-05 13:59:49 +0800

    lhj

--- src/components/App/DocumentInfo.vue
+++ src/components/App/DocumentInfo.vue
@@ -1,21 +1,86 @@
 <template>
   <div id="documentInfo">
-    <div id="left">
-      left
-    </div>
-    <div id="right">
-      right
-    </div>
+    <Row>
+        <Col span="6">
+            <Menu  id="left" :open-names="['1']" active-name="1-2">
+            </Menu>
+            <Menu  id="left" :open-names="['弹性云主机']"  @on-select="push">
+                <Submenu v-for="(item,index) in nav" :key="index" :name="item.name">
+                    <template slot="title">
+                        {{item.name}}
+                    </template>
+                    <MenuItem v-for="(secitem,index) in item.secNav" :key="index" :name="secitem.url">{{secitem.name}}</MenuItem>
+                </Submenu>
+               
+            </Menu>
+        </Col>
+       <col span="18" id="right">
+       </col>
+    </Row>
+    <br>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  export default{}
+import componentTest from './componentTest'
+export default {
+    data () {
+        return {
+            nav: [
+                {
+                    name: '弹性云主机',
+                    default: '产品描述',
+                    secNav: [
+                        {
+                            name: '产品描述',
+                            url: 'computed/1-1'
+                        }
+                    ]
+                },
+                {
+                    name: '快速入门',
+                    secNav: [
+                        {
+                            name: '概述',
+                            url: 'ruicloud/computed/2-1'
+                        },
+                        {
+                            name: '注册及登陆',
+                            url: 'ruicloud/computed/3-1'
+                        },
+                        {
+                            name: '新建主机',
+                            url: 'ruicloud/computed/4-1'
+                        },
+                        {
+                            name: '连接主机',
+                            url: 'ruicloud/computed/5-1'
+                        }
+                    ]
+                }
+            ]
+        }
+    },
+    methods: {
+        push (name) {
+            this.$router.push(name)
+        }
+    },
+    computed: {
+    //   activeName: function () {
+    //     var that = this;
+    //     return that.$router.name;
+    //   }
+    },
+    components: {
+        componentTest
+    }
+}
 </script>
 
 <style rel="stylesheet/less" lang="less" scoped>
-  #documentInfo {
-    width: 1200px;
-    margin: 0px auto;
-  }
+#documentInfo {
+  width: 1200px;
+  margin: 0px auto;
+}
 </style>
---
+++ src/components/App/componentTest.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="page">
+    <slot name="header">默认头部</slot>
+    <button @click="count++">{{count}}</button>
+    <span>{{title11}}</span>
+    <slot name="footer"></slot>
+    <slot></slot>
+  </div>
+</template>
+<script type="text/ecmascript-6">
+export default {
+  data () {
+    return {
+      count: 0
+    }
+  },
+  // props: ['title11'],
+  props: {
+    title11: {
+      required: false,
+      type: String,
+      default: "awq",
+    },
+  },
+  created () {
+
+  },
+  mounted () {
+
+  },
+  methods: {
+
+  },
+  computed: {
+
+  },
+  watch: {
+
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style rel="stylesheet/less" lang="less" scoped>
+</style>
