0cdef3f1ca2ea4744ee425764aef899d7f4f80a7
heyu <784242998@qq.com>
2018-06-06 09:18:12 +0800

    smlj

--- src/components/App/active/smlj.vue
+++ src/components/App/active/smlj.vue
@@ -27,18 +27,23 @@
 
             </div>
             <div class="foot">
-              <button>立即试用</button>
               <p><span>30天</span>免费</p>
-              <p style="font-size:14px;color:rgba(102,102,102,1);margin-top: 10px;">原价：￥{{computerInfo.cost}}起</p>
+              <p style="font-size:14px;color:rgba(102,102,102,1);margin-top: 10px;text-decoration: line-through;">原价：￥{{computerInfo.cost}}起</p>
             </div>
           </div>
           <div class="right">
             <p>续费五折优惠</p>
+            <div class="free">
+              <p>5折</p>
+              <span>续费优惠</span>
+            </div>
           </div>
+        </div>
+        <div class="btn">
+          <button @click="receive">一键领取主机+优惠券</button>
         </div>
       </div>
     </div>
-
     <div class="active-desc">
       <p class="header">活动规则</p>
       <p class="main">1.活动时间： 即日起活动长期有效，数量有限，送完为止</p>
@@ -54,12 +59,12 @@
       <p class="main">5.为保证活动的公平公正，新睿云有权对恶意刷抢（如通过程序等技术手段）活动资源、领取后3天内未使用资源、及利用资源从事违法违规行为的用户收回免费套餐使用及5折优惠续费资格</p>
       <p class="main">6.活动最终解释权在法律范围内归新睿云所有</p>
     </div>
-
-    <Modal v-model="loginModal" width="420" class="login-modal" :scrollable="true">
+    <!--登录-->
+    <Modal v-model="loginModal" width="420" class="login-modal" :scrollable="true" :mask-closable="false">
       <p slot="header" style="color:#5F5F5F;text-align:center;height: 30px;padding-top: 5px;">
         <span style="font-family: PingFangSC-Regular;font-size: 26px;">登录</span>
       </p>
-      <div class="body">
+      <div class="modal-body">
         <form>
           <div>
             <span :class="{warning:vailForm.loginname.warning}">{{vailForm.loginname.message}}</span>
@@ -81,7 +86,7 @@
           </div>
         </form>
       </div>
-      <div slot="footer" class="foot">
+      <div slot="footer" class="modal-foot">
         <button :class="{disabled:disabled}" :disabled="disabled==true" @click="submit">登录</button>
         <div>
           <!--span class="checkBox" :class="{agree:agree}" @click="toggle"></span>&nbsp;<span>我已阅读并同意</span><span
@@ -93,6 +98,30 @@
             忘记密码
           </router-link>
         </div>
+      </div>
+    </Modal>
+    <!--领取失败-->
+    <Modal v-model="modal2" width="550" :scrollable="true" :mask-closable="false" class="smlj-bag">
+      <div style="text-align:center">
+        <p style="color:#E45543;font-size: 24px;margin: 100px 0 40px 0;">抱歉</p>
+        <p style="font-size:18px;color:rgba(51,51,51,1);">{{failMsg}}</p>
+      </div>
+      <div slot="footer" style="text-align: center;margin: 40px 0 110px 0;">
+        <button  class="check" @click="modal2=false">
+          知道了
+        </button>
+      </div>
+    </Modal>
+    <!--领取成功-->
+    <Modal v-model="modal1" width="550" :scrollable="true" :mask-closable="false" class="smlj-bag">
+      <div style="text-align:center">
+        <p style="color:#E45543;font-size: 24px;margin: 100px 0 40px 0;">领取成功</p>
+        <p style="font-size:18px;color:rgba(51,51,51,1);">{{sucessMsg}}</p>
+      </div>
+      <div slot="footer" style="text-align: center;margin: 40px 0 110px 0;">
+        <button  class="check" @click="modal1=false">
+          立即查看
+        </button>
       </div>
     </Modal>
   </div>
@@ -117,6 +146,12 @@
     data() {
       window.scrollTo(0, 0);
       return {
+        // 一键领取弹窗
+        sucessMsg:'',
+        failMsg:'',
+        modal1:false,
+        modal2:false,
+
         loginModal: false,
         form: {
           loginname: '',
@@ -232,7 +267,7 @@
         }
       },
       submit() {
-        this.$http.get('user/login.do', {
+        axios.get('user/login.do', {
           params: {
             username: this.form.loginname,
             password: this.form.password,
@@ -252,11 +287,31 @@
         )
         ;
       },
-      buyNow_one(item, index) {
-        if (this.userInfo == null) {
+//      buyNow_one(item, index) {
+//        if (this.userInfo == null) {
+//          this.loginModal = true
+//          return
+//        }
+//      },
+      // 一键领取主机
+      receive(){
+        if (this.$store.state.userInfo == null) {
           this.loginModal = true
           return
         }
+        axios.get('activity/takeSaleMv.do',{
+            params:{
+              token:this.$route.query.token
+            }
+        }).then(response =>{
+          if (response.status == 200 && response.data.status == 1){
+              this.sucessMsg=response.data.message
+               this.modal1=true
+          }else{
+            this.failMsg=response.data.message
+            this.modal2=true
+          }
+        })
       },
       setData(result){
         this.computerInfo.cost = result.data.cost;
@@ -282,11 +337,11 @@
     .active-body {
       width: 1200px;
       margin: 0px auto;
+      box-shadow: 0px 2px 50px 0px rgba(245, 127, 114, 0.48);
+      background-color: #fff;
       .body {
-        padding: 60px 100px;
+        padding: 60px 100px 20px 100px;
         display: flex;
-        box-shadow: 0px 2px 50px 0px rgba(245, 127, 114, 0.48);
-        background-color: #fff;
         .left {
           width: 60%;
           padding: 20px 10% 20px 0px;
@@ -322,9 +377,10 @@
                 vertical-align: bottom;
                 content: '';
                 display: inline-block;
-                width: 18px;
-                height: 18px;
-                background-color: #00c1de;
+                width: 15px;
+                height: 15px;
+                background-color: #F26667;
+                margin-right: 10px;
               }
             }
           }
@@ -334,18 +390,6 @@
               span {
                 color: #E45543
               }
-            }
-            button {
-              float: right;
-              padding: 5px 20px;
-              border-radius: 15px;
-              outline: none;
-              border: none;
-              background-color: #E45543;
-              color: #fff;
-              font-size: 14px;
-              cursor: pointer;
-              margin-top: 5px;
             }
           }
         }
@@ -359,6 +403,45 @@
             color: rgba(228, 85, 67, 1);
             line-height: 18px;
           }
+          .free{
+            background: url('../../../assets/img/active/smlj/free.png') no-repeat center;
+            margin-top: 20px;
+            text-align: center;
+            p{
+              position: relative;
+              left: 28px;
+              font-size:50px;
+              color:rgba(255,255,255,1);
+              line-height:70px;
+            }
+            span{
+              position: relative;
+              left: 28px;
+              font-size:18px;
+              color:rgba(255,255,255,1);
+              line-height:38px;
+            }
+          }
+        }
+      }
+      .btn{
+        width: 1000px;
+        margin: 0 auto;
+        text-align: center;
+        border-top:2px solid rgba(153,153,153,0.3);
+        padding: 18px 0;
+        button{
+          width:260px;
+          height:42px;
+          background:rgba(228,85,67,1);
+          border-radius:21px;
+          font-size:18px;
+          font-family:PingFangSC-Regular;
+          color:rgba(255,255,255,1);
+          line-height:18px;
+          border: none;
+          outline: none;
+          cursor: pointer;
         }
       }
     }
@@ -397,6 +480,151 @@
         line-height: 28px;
       }
     }
+  }
+  .modal-body {
+    height: 55%;
+    form {
+      margin-top: 15px;
+    }
+    input {
+      border: none;
+      border-bottom: 1px solid #ccc;
+      outline: none;
+      background: rgba(0, 0, 0, 0);
+      height: 50px;
+      font-size: 14px;
+      width: 80%;
+      margin: 0px auto;
+      display: block;
+      margin-bottom: 20px;
+      &:-webkit-autofill {
+        -webkit-box-shadow: 0 0 0px 1000px white inset;
+      }
+      &::-webkit-input-placeholder { /* Chrome/Opera/Safari */
+        font-family: PingFangSC-Regular;
+        font-size: 14px;
+        color: #B6B6B6;
+      }
+      &::-moz-placeholder { /* Firefox 19+ */
+        font-family: PingFangSC-Regular;
+        font-size: 14px;
+        color: #B6B6B6;
+      }
+      &:-ms-input-placeholder { /* IE 10+ */
+        font-family: PingFangSC-Regular;
+        font-size: 14px;
+        color: #B6B6B6;
+      }
+      &:-moz-placeholder { /* Firefox 18- */
+        font-family: PingFangSC-Regular;
+        font-size: 14px;
+        color: #B6B6B6;
+      }
+    }
+    span {
+      display: block;
+      width: 80%;
+      margin: 0px auto;
+      font-family: PingFangSC-Regular;
+      font-family: PingFangSC-Regular;
+      font-size: 14px;
+      color: #B6B6B6;
+      font-size: 14px;
+      line-height: 14px;
+      height: 14px;
+      transition: all .5s;
+      &.warning {
+        color: #F24747;
+      }
+    }
+    img {
+      width: 80px;
+      height: 30px;
+      position: absolute;
+      display: block;
+      bottom: 12px;
+      right: 43px;
+      cursor: pointer;
+      //background: #4990E2;
+      //border: 1px solid white;
+      border-radius: 3px;
+      font-family: PingFangSC-Regular;
+      font-size: 11px;
+      color: #FFFFFF;
+      letter-spacing: 0.71px;
+      outline: none;
+    }
+  }
+
+  .modal-foot {
+    padding-top: 10px;
+    height: 32.1%;
+    button {
+      width: 80%;
+      margin: 0px auto;
+      display: block;
+      height: 45px;
+      background-color: #4990E2;
+      border: none;
+      font-family: PingFangSC-Medium;
+      font-size: 14px;
+      color: #FFFFFF;
+      letter-spacing: 0.83px;
+      cursor: pointer;
+      margin-bottom: 15px;
+      &.disabled {
+        cursor: not-allowed;
+      }
+    }
+    .checkBox {
+      width: 12px;
+      height: 12px;
+      border-radius: 2px;
+      display: inline-block;
+      border: 1px solid #ccc;
+      cursor: pointer;
+    }
+    .agree {
+      background-color: #2d8cf0;
+      border-color: #2d8cf0;
+      position: relative;
+      &::after {
+        content: "";
+        display: table;
+        width: 4px;
+        height: 8px;
+        position: absolute;
+        top: 0px;
+        left: 3px;
+        border: 2px solid #fff;
+        border-top: 0;
+        border-left: 0;
+        transform: rotate(45deg) scale(1);
+      }
+    }
+    div {
+      width: 80%;
+      height: 35px;
+      margin: 0px auto;
+    }
+    span {
+      vertical-align: middle;
+      font-family: PingFangSC-Regular;
+      font-size: 14px;
+      letter-spacing: 0.83px;
+    }
+  }
+
+  .check{
+   width:115px;
+    height:38px;
+    font-size:18px;
+    background:rgba(228,85,67,1);
+    box-shadow:0px 7px 26px -10px rgba(242,102,103,1);
+    border-radius:19px;
+    color:#FFF;
+    border: none;
+    outline: none;
   }
 </style>
 
