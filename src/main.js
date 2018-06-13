7f46d5083513d4e1aab0edd50e7d329ca673d92f
eCheo <e1511404018@qq.com>
2018-06-13 10:20:02 +0800

    'deng'

--- src/main.js
+++ src/main.js
@@ -39,7 +39,7 @@ axios.defaults.baseURL = 'http://192.168.3.109:8080/ruirados/'
 
 
 // //axios.defaults.baseURL = 'https://pan.xrcloud.net/ruicloud/'
-axios.defaults.withCredentials = true
+
 
 // axios挂载到Vue原型
 Vue.prototype.$http = axios.create({
