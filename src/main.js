0418ab429e71f9a5a96a05b7ce1ed89ba2f755df
tanchengwei <804589369@qq.com>
2018-06-22 18:00:52 +0800

    file

--- src/main.js
+++ src/main.js
@@ -34,10 +34,10 @@ Vue.config.productionTip = false
 //  axios.defaults.baseURL = 'https://pan.xrcloud.net/ruicloud/'
 
 
-axios.defaults.baseURL = 'http://192.168.3.109:8080/ruirados/'
+//axios.defaults.baseURL = 'http://192.168.3.109:8080/ruirados/'
 // axios.defaults.baseURL = 'http://192.168.3.229:8080/ruirados/'
 //axios.defaults.baseURL = 'https://pan.xrcloud.net/ruicloud/'
-axios.defaults.withCredentials = true
+//axios.defaults.withCredentials = true
 
 // axios挂载到Vue原型
 Vue.prototype.$http = axios.create({
