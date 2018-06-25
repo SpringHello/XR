a46ecfb9a7ca91bc4d1f1fd80e428ea038f9bcb0
qrl <920149862@qq.com>
2018-06-25 09:35:35 +0800

    'q'

--- src/main.js
+++ src/main.js
@@ -35,13 +35,13 @@ Vue.config.productionTip = false
 //axios.defaults.baseURL = 'http://192.168.3.124:8082/ruicloud'
 //axios.defaults.baseURL = 'http://192.168.3.204:8081/ruicloud'
 
-axios.defaults.baseURL = 'http://192.168.3.105:8082/ruicloud/'
+//axios.defaults.baseURL = 'http://192.168.3.105:8082/ruicloud/'
 
 // axios.defaults.baseURL = 'https://pan.xrcloud.net/ruicloud/'
 
 
 
-//axios.defaults.baseURL = 'http://localhost:8082/ruicloud/'
+axios.defaults.baseURL = 'http://localhost:8082/ruicloud/'
 
 //axios.defaults.baseURL = 'http://192.168.3.105:8082/ruicloud/'
 
