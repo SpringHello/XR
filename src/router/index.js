115052f70dbb250f92cae23130dfa0e8977e664c
lhj <2420479720@qq.com>
2018-08-07 15:07:48 +0800

    east-south-activity end

--- src/router/index.js
+++ src/router/index.js
@@ -43,6 +43,7 @@ const GroupBooking = () => import('@/components/App/groupbooking/homePage')
 const ProductShare = () => import('@/components/App/groupbooking/sharePage')
 const Activity = () => import('@/components/App/groupbooking/activityPage')
 const FrActive = () => import('@/components/App/active/freeReceive')
+const EastSouthNode = () => import('@/components/App/active/EastSouthNode')
 
 
 // 备案系统页面路由
@@ -254,6 +255,7 @@ var router = new Router({
         {path: 'active_2', name: 'active_2', component: active_2},
         {path: 'smlj', name: 'smlj', component: smlj},
         {path: 'frActive', name: 'frActive', component: FrActive},
+        {path: 'EastSouthNode', name: 'EastSouthNode', component: EastSouthNode},
         {path: 'Pecs', component: Pecs},
         {path: 'Phost', component: Phost},
         {path: 'Pecss', component: Pecss},
