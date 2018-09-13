0a6d95940be7e93ddf9bfe12a33ae105afca920f
lhj <2420479720@qq.com>
2018-09-13 19:35:32 +0800

    home m 2

--- src/components/App/Home.vue
+++ src/components/App/Home.vue
@@ -454,9 +454,9 @@
         eigthSceneMenu: [
           {name: '云电脑', link: 'host'},
           {name: '自助建站', link: 'web'},
-          {name: '个人网盘', link: 'disk'},
+          {name: '存储&网盘', link: 'disk'},
           {name: '软件研发', link: 'software'},
-          {name: '云游戏服务', link: 'game'},
+          {name: '游戏服务', link: 'game'},
           {name: '图形设计', link: 'design'},
           {name: '人工智能', link: 'AI'},
           {name: '超级运算', link: 'supercomputing'}
@@ -466,22 +466,99 @@
             title: '云电脑',
             scene: '云电脑是由新睿云所提供的云上虚拟Windows桌面服务，为用户提供随时随地高效接入PC的便利。云电脑可按需申请轻松使用，云电脑助您打造更精简、更安全、更低维护成本、更高服务效率的个人PC使用系统。借助新睿云，无论您使用何种终端设备，云电脑都可以让您拥有完整的高性能PC使用体验。云电脑为您提供持续、安全、稳定、高性价比的BYOD模式云端计算服务。',
             software: [
-              require('../../assets/img/home/eightscene-host/software-1.png'),
-              require('../../assets/img/home/eightscene-host/software-2.png'),
-              require('../../assets/img/home/eightscene-host/software-3.png'),
-              require('../../assets/img/home/eightscene-host/software-4.png')
+              require('../../assets/img/sceneList/sl-icon1.png'),
+              require('../../assets/img/sceneList/sl-icon2.png'),
+              require('../../assets/img/sceneList/sl-icon3.png'),
+              require('../../assets/img/sceneList/sl-icon4.png')
             ],
             configure: ['1核1G1M带宽'],
             link: 'host',
-            bgUrl: require('../../assets/img/home/eightscene-host/bg.png')
+            bgUrl: require('../../assets/img/home/eightscene-bg-1.png')
           },
           {
             title: '自助建站',
-            scene: '云电脑是由新睿云所提供的云上虚拟Windows桌面服务，为用户提供随时随地高效接入PC的便利。云电脑可按需申请轻松使用，云电脑助您打造更精简、更安全、更低维护成本、更高服务效率的个人PC使用系统。借助新睿云，无论您使用何种终端设备，云电脑都可以让您拥有完整的高性能PC使用体验。云电脑为您提供持续、安全、稳定、高性价比的BYOD模式云端计算服务。',
-            software: [],
+            scene: '基于新睿云自助建站服务，您能够轻松获得一站式的建站服务方案支持。包括配置好各类开源建站镜像模板，帮助您快速搭建网站、微博、论坛等服务内容；可无限扩容的对象存储OSS服务帮助您存储网站内容与数据；域名购买注册系统、备案系统等一套完整的服务流程。并且因所有内容部署于云端，您可以根据内容与实际需要，弹性调整您的计算规格与资源大小，合理控制预算，以极具性价比的方式完成个人网站搭建。',
+            software: [
+              require('../../assets/img/sceneList/sl-icon5.png'),
+              require('../../assets/img/sceneList/sl-icon6.png'),
+              require('../../assets/img/sceneList/sl-icon7.png'),
+              require('../../assets/img/sceneList/sl-icon8.png')
+            ],
             configure: ['1核1G1M带宽'],
-            link: 'web'
-          }
+            link: 'web',
+            bgUrl: require('../../assets/img/home/eightscene-bg-2.png')
+          },
+          {
+            title: '存储&网盘',
+            scene: '独立服务模式数据安全存储、多终端同步、文件在线预览，个人网盘让您的数据存储于传输有更简单可靠的选择。新睿云个人网盘服务具有使用方便、管理简单、稳定可靠等特点。具备多协议支持能力（包括FTP、FTPS、SFTP等文件传输协议）；远程文件查找能力；多标签界面管理能力；断点续传；站点管理与传输队列管理等能力。',
+            software: [
+              require('../../assets/img/sceneList/sl-icon9.png'),
+              require('../../assets/img/sceneList/sl-icon10.png')
+            ],
+            configure: ['1核1G1M带宽'],
+            link: 'disk',
+            bgUrl: require('../../assets/img/home/eightscene-bg-3.png')
+          },
+          {
+            title: '软件研发',
+            scene: '基于新睿云云主机的集成开发环境包含jave的Eclipse、PHP的Sublime、PHPstrom、Python的PyCharm。帮助您快速部署开发环境与依赖包，降低开发前期准备并提升研发效率。借助新睿云成熟的云计算基础服务能力，开发人员可以随时创建与释放所需的开发环境与相关计算资源，主机信息可自动与手动备份，多种方式保障您的数据安全。',
+            software: [
+              require('../../assets/img/sceneList/sl-icon11.png'),
+              require('../../assets/img/sceneList/sl-icon12.png'),
+              require('../../assets/img/sceneList/sl-icon13.png'),
+              require('../../assets/img/sceneList/sl-icon14.png')
+            ],
+            configure: ['1核1G1M带宽'],
+            link: 'software',
+            bgUrl: require('../../assets/img/home/eightscene-bg-4.png')
+          },
+          {
+            title: '游戏服务',
+            scene: '基于新睿云云主机的集成开发环境包含jave的Eclipse、PHP的Sublime、PHPstrom、Python的PyCharm。帮助您快速部署开发环境与依赖包，降低开发前期准备并提升研发效率。借助新睿云成熟的云计算基础服务能力，开发人员可以随时创建与释放所需的开发环境与相关计算资源，主机信息可自动与手动备份，多种方式保障您的数据安全。',
+            software: [
+              require('../../assets/img/sceneList/sl-icon15.png'),
+              require('../../assets/img/sceneList/sl-icon16.png')
+            ],
+            configure: ['1核1G1M带宽'],
+            link: 'game',
+            bgUrl: require('../../assets/img/home/eightscene-bg-5.png')
+          },
+          {
+            title: '图形设计',
+            scene: '工程制图、游戏、电影领域有大量需要高计算量的场景需要企业或个人投入较高成本来提高生产计算能力和生产效率。现在借助新睿云3D设计，以远低于自购设备的价格来获取更为稳定优质的计算资源能力提升，成倍提高您的设计工作效率和渲染效率。并且，新睿云3D设计提供按需付费方式，以小时级为单位来获取工业级GPU计算服务能力。',
+            software: [
+              require('../../assets/img/sceneList/sl-icon17.png'),
+              require('../../assets/img/sceneList/sl-icon18.png'),
+              require('../../assets/img/sceneList/sl-icon19.png'),
+              require('../../assets/img/sceneList/sl-icon20.png')
+            ],
+            configure: ['1核1G1M带宽'],
+            link: 'design',
+            bgUrl: require('../../assets/img/home/eightscene-bg-6.png')
+          },
+          {
+            title: '人工智能',
+            scene: '新睿云人工智能平台基于强劲的 GPU 计算资源，在配置 Tesla P40和 Tesla P100 直通模式的基础上，搭载 Caffe、CNTK、PyTorch 和 Keras 等多个主流深度学习框架，同时集成 Jupyter notebook 开发环境及 numpy、scipy、pandas、等众多数据科学工具包。用户可在新睿云人工智能平台上使用 GPU 或 CPU 进行单机或分布式深度学习模型训练与推断，并可享受云计算弹性特性，按需进行横向、纵向扩展。',
+            software: [
+              require('../../assets/img/sceneList/sl-icon21.png'),
+              require('../../assets/img/sceneList/sl-icon22.png'),
+              require('../../assets/img/sceneList/sl-icon23.png'),
+              require('../../assets/img/sceneList/sl-icon24.png')
+            ],
+            configure: ['1核1G1M带宽'],
+            link: 'AI',
+            bgUrl: require('../../assets/img/home/eightscene-bg-7.png')
+          },
+          {
+            title: '超级运算',
+            scene: '应用场景说明：新睿云深度学习平台基于强劲的 GPU 计算资源，在配置 Tesla P40和 Tesla P100 的基础上。各个行业都可以帮助您成倍提高计算和工作效率。比如在计算机视觉与图像处理领域，图像处理与计算机视觉算法是计算密集型算法。NVIDIA&#174 CUDA&#174 加速技术可帮助解决该挑战，从而帮助此类应用程序实现交互式视频的帧率性能。用户可在深度学习平台上使用 GPU 或 CPU 进行单机或分布式深度学习模型训练与推断，并可享受云计算弹性特性，按需进行横向、纵向扩展。 ',
+            software: [
+              require('../../assets/img/sceneList/sl-icon25.png')
+            ],
+            configure: ['1核1G1M带宽'],
+            link: 'supercomputing',
+            bgUrl: require('../../assets/img/home/eightscene-bg-8.png')
+          },
         ],
         bannerText: require('../../assets/img/home/active3Text.png'),
         bannerImages: [
@@ -1113,6 +1190,8 @@
               color:rgba(74,74,74,1);
             }
             .scene {
+              max-height: 132px;
+              overflow: hidden;
               margin-bottom: 24px;
               div {
                 p {
