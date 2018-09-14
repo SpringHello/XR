07251383829fab0f136524c57410cb924fc3ff09
qrl <920149862@qq.com>
2018-09-14 12:58:13 +0800

    'q'

--- src/components/App/SceneInfo.vue
+++ src/components/App/SceneInfo.vue
@@ -1820,12 +1820,22 @@
           },
           {
             title: '押金金额',
+            width: 130,
             render: (h, params) => {
-              return h('span', {
+              let arr = []
+              let param1 = h('li', {
+                style: {
+                  textDecoration: 'line-through'
+                }
+              }, '原价：¥' + params.row.originalPrice)
+              let param2 = h('li', {
                 style: {
                   color: '#D0021B'
                 }
               }, '¥' + params.row.cashPledge)
+              arr.push(param1)
+              arr.push(param2)
+              return h('ul', {}, arr)
             }
           },
         ],
@@ -1984,6 +1994,7 @@
                 this.orderData.push({
                   productType: '云服务器',
                   configs: this.currentSceneGroup[this.index1].configGroup[this.index2].configs,
+                  originalPrice: this.currentSceneGroup[this.index1].configGroup[this.index2].originalPrice,
                   time: this.time,
                   cashPledge: Number(this.cashPledge)
                 })
