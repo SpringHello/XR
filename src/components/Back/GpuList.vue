ebde3f3d26b59b8147af55897404a099b3f6d213
eCheo <e1511404018@qq.com>
2018-09-26 15:05:16 +0800

    'd'

--- src/components/Back/GpuList.vue
+++ src/components/Back/GpuList.vue
@@ -866,9 +866,9 @@
         },
       },
       created(){
-        // this.intervalInstance = setInterval(() => {
-        //   this.getGpuServerList()
-        // }, 5 * 1000)
+        this.intervalInstance = setInterval(() => {
+          this.getGpuServerList()
+        }, 5 * 1000)
       },
       mounted(){
           this.getGpuServerList();
