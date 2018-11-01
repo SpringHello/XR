87d464386088b64ff4de20ff149966befa6e6e04
heyu <784242998@qq.com>
2018-11-01 14:48:53 +0800

    file

--- src/main.js
+++ src/main.js
@@ -39,7 +39,7 @@ Vue.config.productionTip = false
 
 
 
-axios.defaults.baseURL = 'https://dev.xrcloud.net/ruicloud/'
+axios.defaults.baseURL = 'https://zschj.xrcloud.net/ruicloud/'
 // axios.defaults.baseURL = '/ruicloud'
 
 
