62738a698354d731feabb306f1199296ca57713c
lhj <2420479720@qq.com>
2018-11-13 19:21:03 +0800

    beforeactive m

--- src/components/App/active/anniversaryActiveBefore.vue
+++ src/components/App/active/anniversaryActiveBefore.vue
@@ -214,15 +214,24 @@
           <img style="margin-left: 120px" src="../../../assets/img/active/anniversary/aa-banner21.png"/>
           <p><span>购买云产品获赠好礼，最高额消费可领全部礼品</span></p>
         </div>
-        <div class="send-full w12">
+        <!-- <div class="send-full w12">
           <ul v-for="(item,index) in sendFullList">
             <li><p :class="{'onStep': true}">消费满<span> {{ item.text_1}} </span>可领</p></li>
             <li><i :class="{'onStep': true}"></i></li>
             <li><i :class="{'onStep': true}"></i></li>
             <li><img :src="item.src_2"/></li>
             <li :class="{'onStep': true}">{{ item.text_2}}</li>
-            <!-- <button @click="getSendFull(index)" :class="{'disabled': true}" :disabled="true">立即领取</button> -->
             <div v-if="index !=3" class="send-full-dashed"></div>
+          </ul>
+        </div> -->
+        <div class="send-full w12">
+          <ul v-for="(item,index) in sendFullList">
+            <li><p :class="{'onStep': index >= spentCostNode}">消费满<span> {{ item.text_1}} </span>可领</p></li>
+            <li><span :class="{'onStep': index >= spentCostNode}">{{ item.text_2 }}</span></li>
+            <li style="margin-top: 30px"><p style="position: relative;right: 18px" v-if="index >= spentCostNode"><img src="../../../assets/img/active/anniversary/aa-icon17.png"/> <img src="../../../assets/img/active/anniversary/aa-icon18.png"/></p>
+              <p style="position: relative;right: 18px" v-else><img src="../../../assets/img/active/anniversary/aa-icon19.png"/> <img src="../../../assets/img/active/anniversary/aa-icon20.png"/></p></li>
+            <li><img v-if="index >= spentCostNode" :src="item.src_2"/><img v-else :src="item.src_1"/></li>
+            <div v-if="index !=3" class="send-full-dashed" :class="{'onStep': true}"></div>
           </ul>
         </div>
         <button class="activecenter-btn" @click="$router.push('activecenter')">点击查看其他优惠活动</button>
@@ -299,6 +308,7 @@
           discountRuleModal: false,
           activeStart: false,
         },
+        spentCostNode: 0,
         aa_scrollTop: 0,
         ruleShow: false,
         day: '--',
@@ -1356,6 +1366,12 @@
             font-family: MicrosoftYaHei;
             font-weight: 600;
             color: #666666;
+            >img{
+              vertical-align: middle;
+            }
+            img:nth-child(1){
+              margin-right: 14px;
+            }
             > span {
               font-size: 32px;
             }
@@ -1363,21 +1379,13 @@
               color: #FF3000;
             }
           }
-          > i {
-            &:before {
-              content: '';
-              display: inline-block;
-              height: 15px;
-              width: 15px;
-              transform: rotate(-45deg);
-              border-right: 1px solid #666;
-              border-top: 1px solid #666;
-            }
+          > span {
+            font-size: 20px;
+            font-family: MicrosoftYaHei;
+            font-weight: 600;
+            color: #666666;
             &.onStep {
-              &:before {
-                border-right: 1px solid #FF3000;
-                border-top: 1px solid #FF3000;
-              }
+              color: #FF3000;
             }
           }
         }
@@ -1386,7 +1394,6 @@
         }
         li:nth-child(4) {
           position: relative;
-          bottom: 35px;
         }
         li:nth-child(5) {
           font-size: 18px;
@@ -1420,12 +1427,26 @@
       }
       .send-full-dashed {
         position: absolute;
-        width: 170px;
-        border: 1px dashed #C0C0C0;
-        top: 148px;
-        left: 220px;
+        width: 15px;
+        height: 15px;
+        border-right: 1px solid #C0C0C0;
+        top: 212px;
+        left: 285px;
+        &:before {
+          display: inline-block;
+          content: '';
+          height: 15px;
+          width: 15px;
+          border-top: 1px solid #C0C0C0;
+          position: relative;
+          left: 7px;
+          top: 7px;
+        }
         &.onStep {
-          border: 1px dashed #FF3000;
+          border-right: 1px solid #FF3000;
+          &:before {
+            border-top: 1px solid #FF3000;
+          }
         }
       }
     }
@@ -1469,7 +1490,7 @@
     }
   }
   .activecenter-btn {
-    margin-top: 30px;
+    margin-top: 60px;
     width: 227px;
     height: 36px;
     background: #ff915c;
