0ac0a74c85a99adc0097370c96bf81c798f74f34
lhj <2420479720@qq.com>
2018-11-13 20:02:45 +0800

    lhj

--- src/components/App/active/anniversaryActiveBefore.vue
+++ src/components/App/active/anniversaryActiveBefore.vue
@@ -2,6 +2,9 @@
   <div id="anniversary-active-before">
     <div class="banner center">
       <div class="content">
+        <div class="hint-wrap">
+          <p>登录即可参与抽奖(100%中奖)，戴森(Dyson)，黑莓等你来拿！</p>
+        </div>
         <div class="timer">
           <h3>距离活动开启还剩</h3>
           <div class="countdown center">
@@ -44,11 +47,11 @@
       <div class="content center">
         <div class="products-title tl" style="margin:0 24px;">
           <img src="../../../assets/img/active/anniversary/aa-icon6.png"/>
-          <span>云数据库限时抢购中</span>
-          <div style="margin-top: 8px">
+          <span>云主机限时抢购中</span>
+          <!-- <div style="margin-top: 8px">
             <span>本场结束倒计时:</span>
             <p><span>{{ hour}} : {{ minute}} : {{second}}</span></p>
-          </div>
+          </div> -->
         </div>
         <ul class="item">
           <li v-for="(item,index) in hostData" :key="index">
@@ -111,10 +114,10 @@
           <div class="gpu-title tl">
             <img src="../../../assets/img/active/anniversary/aa-icon8.png"/>
             <span>GPU云服务器 每日19:00开启抢购</span>
-            <div style="margin-top: 8px">
+            <!-- <div style="margin-top: 8px">
               <span>本场结束倒计时:</span>
               <p><span>{{ hour }} : {{ minute }} : {{ second }}</span></p>
-            </div>
+            </div> -->
           </div>
           <div style="height:4px;background:rgba(255,108,62,1);margin-top: 20px"></div>
           <div class="gpu-item">
@@ -332,7 +335,7 @@
             zone: '北方一区',
             system: 'windows2012-64',
             sectime: '3',
-            cost: '1131.16',
+            cost: '1331.16',
             discount: '226.3',
             cpu: '4',
             memroy: '8',
@@ -637,10 +640,26 @@
         rgba(254, 237, 225, 1)
       );
       .content {
-        background: url("../../../assets/img/active/anniversary/aa-banner2.png") center no-repeat,url("../../../assets/img/active/anniversary/aa-banner1.png") center 0px no-repeat;
+        background: url("../../../assets/img/active/anniversary/aa-banner2.png") center no-repeat,url("../../../assets/img/active/anniversary/aa-banner1.png") center 60px no-repeat;
         height: 844px;
+        > .hint-wrap {
+          padding-top: 45px;
+           p {
+            background: #FA531C;
+            padding: 8px 26px 8px 21px;
+            border: 2px solid rgba(255, 215, 78, 1);
+            border-radius: 19px;
+            font-size: 20px;
+            font-family: MicrosoftYaHei;
+            font-weight: 400;
+            color: rgba(253, 253, 253, 1);
+            width: 629px;
+            margin: 0 auto;
+          }
+        }
+        
         .timer {
-          padding-top: 650px;
+          padding-top: 620px;
           h3 {
             font-size: 30px;
             font-family: PingFangSC-Semibold;
