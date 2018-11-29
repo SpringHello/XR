eb6ad576f35311d1dff1410b52dd3335a6fd932a
qrl <920149862@qq.com>
2018-11-29 20:55:35 +0800

    'q'

--- src/Back.vue
+++ src/Back.vue
@@ -254,8 +254,8 @@
             mainName: '云存储',
             type: 'storage',
             subItem: [
-              //{subName: '对象存储', type: 'https://oss-console.xrcloud.net/ruirados/objectStorage'},
-              {subName: '对象存储', type: 'https://testoss-console.xrcloud.net/ruirados/objectStorage'},
+              {subName: '对象存储', type: 'https://oss-console.xrcloud.net/ruirados/objectStorage'},
+              //{subName: '对象存储', type: 'https://testoss-console.xrcloud.net/ruirados/objectStorage'},
               {subName: '云硬盘', type: 'disk'},
               {subName: '云硬盘备份', type: 'diskBackup'}
               /* {subName: '硬盘快照', type: 'diskSnapshot'} */
@@ -291,8 +291,8 @@
             mainName: '域名服务',
             type: 'domain',
             subItem: [
-              {subName: '域名管理', type: 'https://test-domain.xrcloud.net/xrdomain/domainGroup'},
-              {subName: '信息模版', type: 'https://test-domain.xrcloud.net/xrdomain/domainInfoTemplate'},
+              {subName: '域名管理', type: 'https://domain.xrcloud.net/xrdomain/domainGroup'},
+              {subName: '信息模版', type: 'https://domain.xrcloud.net/xrdomain/domainInfoTemplate'},
               // {subName: '域名转入', type: 'https://test-domain.xrcloud.net/xrdomain/domainTransfer'},
               /*{subName: '邮箱验证', type: 'https://test-domain.xrcloud.net/xrdomain/domainGroup'}*/
             ]
