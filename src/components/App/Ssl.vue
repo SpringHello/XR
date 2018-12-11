90004f613c56b53dfd869ca17f2b37afa7c64925
lhj <2420479720@qq.com>
2018-12-11 18:04:39 +0800

    121

--- src/components/App/Ssl.vue
+++ src/components/App/Ssl.vue
@@ -12,25 +12,21 @@
     </div>
     <div class="brand">
       <div class="wrap">
-        <div class="top"></div>
+        <div class="headline">
+          <h1>SSL证书品牌</h1>
+        </div>
         <div class="container"></div>
       </div>
     </div>
     <div class="advantage">
       <div class="wrap">
-        <div class="top"></div>
+        <div class="headline">产品优势</div>
         <div class="container"></div>
       </div>
     </div>
     <div class="scene">
       <div class="wrap">
-        <div class="top"></div>
-        <div class="container"></div>
-      </div>
-    </div>
-    <div class="box">
-      <div class="wrap">
-        <div class="top"></div>
+        <div class="headline">使用场景</div>
         <div class="container"></div>
       </div>
     </div>
@@ -39,15 +35,15 @@
 
 <script type="text/ecmascript-6">
 export default {
-  data() {
+  data () {
     return {
 
     }
   },
-  created() {
+  created () {
 
   },
-  mounted() {
+  mounted () {
 
   },
   methods: {
@@ -71,14 +67,57 @@ export default {
   margin: 0 auto;
   width: 1200px;
 }
+.headline {
+  text-align: center;
+  h1 {
+    display: inline-block;
+    position: relative;
+    &::before {
+      content: url(../../assets/img/ssl/headline_left_icon.png);
+      position: absolute;
+      top: 0;
+      left:-82px;
+    }
+    &::after {
+      content: url(../../assets/img/ssl/headline_right_icon.png);
+      position: absolute;
+      top: 0;
+      right:-82px;
+    }
+  }
+}
 .banner {
-  background: url('../../assets/img/ssl/banner-bg.png') no-repeat center;
+  background: url("../../assets/img/ssl/banner-bg.png") no-repeat center;
   .wrap {
     height: 400px;
+    padding: 0 50px;
     display: flex;
+    justify-content: space-between;
     align-items: center;
-    div {
-
+    .text {
+      width: 480px;
+      h1 {
+        font-size: 36px;
+      }
+      p {
+        margin: 28px 0 50px;
+        font-size: 16px;
+        line-height: 22px;
+      }
+      button {
+        width: 164px;
+        height: 46px;
+        border-radius: 4px;
+        opacity: 0.9;
+        border: 1px solid rgba(51, 51, 51, 1);
+        font-size: 16px;
+        background: 0;
+        &:hover {
+          background: rgba(255, 57, 42, 1);
+          border: 1px solid rgba(255, 57, 42, 1);
+          color: #fff;
+        }
+      }
     }
   }
 }
