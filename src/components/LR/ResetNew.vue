418311db2d813421e3cdade696a94a98bc3786b6
eCheo <e1511404018@qq.com>
2018-12-26 15:18:17 +0800

    d

--- src/components/LR/ResetNew.vue
+++ src/components/LR/ResetNew.vue
@@ -154,7 +154,7 @@
     let reg = /^1[3|5|8|9|6|7]\d{9}$/;
     if(value == ''){
       return callback(new Error('请输入手机号'));
-    }else if(!reg.rest(value)){
+    }else if(!reg.test(value)){
       return callback(new Error('手机号格式不正确'));
     }else{
       callback();
