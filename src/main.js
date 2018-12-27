3aeb7df80d3ffbf1579c61427e99666fd4840990
qrl <920149862@qq.com>
2018-12-27 21:38:30 +0800

    'q'

--- src/main.js
+++ src/main.js
@@ -48,8 +48,8 @@ Vue.prototype.$throw = (error)=> errorHandler(error,this);
 //axios.defaults.withCredentials = true
 
 // axios.defaults.baseURL = 'https://kaifa.xrcloud.net/ruicloud/'
-axios.defaults.baseURL = 'https://zschj.xrcloud.net/ruicloud/'
-//axios.defaults.baseURL = 'https://www.xrcloud.net/ruicloud/'
+//axios.defaults.baseURL = 'https://zschj.xrcloud.net/ruicloud/'
+axios.defaults.baseURL = 'https://www.xrcloud.net/ruicloud/'
 
 // window.eventBus = new Vue();
 
