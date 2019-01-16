c4f48177374785e1e61b4c47cdabb6b30af471ec
lhj <2420479720@qq.com>
2019-01-16 17:37:27 +0800

    sroll的bug

--- src/components/App/buy/BData.vue
+++ src/components/App/buy/BData.vue
@@ -683,7 +683,7 @@
         prod.cost = this.totalDataCost
         prod.VMName = this.dbName
         this.$parent.cart.push(JSON.parse(JSON.stringify(prod)))
-        window.scrollTo(0, 182)
+        // window.scrollTo(0, 182)
       },
       buyData() {
         if (this.userInfo == null) {
