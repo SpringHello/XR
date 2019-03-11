bc977d808191cd5c87c786c0fdfaf55a145436cd
lhj <2420479720@qq.com>
2019-03-11 17:04:56 +0800

    buy小bug

--- src/components/App/buy/BHost.vue
+++ src/components/App/buy/BHost.vue
@@ -53,8 +53,7 @@
             <div class="item-wrapper">
               <div style="display: flex;justify-content: space-between;">
                 <div v-for="(item,index) in mirrorListQ" :key="index" class="fast-mirror" :class="{'select-fast-mirror':FastMirrorIndex==index}" @click="setOSQ(item,index)">
-                  <img :src="require('../../../assets/img/host/h-icon12.png')" alt="" v-if="index==0||index==1">
-                  <img :src="require(`../../../assets/img/host/h-icon${index+4}.png`)" alt="" v-else>
+                  <img :src="item.img" alt="">
                   <span>{{item.templatename}}</span>
                 </div>
               </div>
@@ -333,7 +332,7 @@
                       {{item.acllistname}}
                     </Option>
                   </Select>
-                  <span style="margin-left:10px;color:#4381EE;font-size:14px;" @click="$router.push('document')">帮助文档</span>
+                  <span style="margin-left:10px;color:rgb(42, 153, 242);font-size:14px;" @click="$router.push('document')">帮助文档</span>
                 </div>
               </div>
             </div>
@@ -807,6 +806,13 @@
         fastCoupon: 0,
         mirrorQuery: this.$route.query.mirror,
         mirrorListQ: [],
+        mirrorListQImg: [
+          {img: require('../../../assets/img/host/h-icon12.png')},
+          {img: require('../../../assets/img/host/h-icon12.png')},
+          {img: require('../../../assets/img/host/h-icon6.png')},
+          {img: require('../../../assets/img/host/h-icon6.png')},
+          {img: require('../../../assets/img/host/h-icon7.png')},
+        ],
       }
     },
     created() {
@@ -835,6 +841,9 @@
         }).then(response => {
           if (response.status == 200 && response.data.status == 1) {
             this.mirrorListQ = response.data.result
+            this.mirrorListQ.forEach((item,index) => {
+              item.img = this.mirrorListQImg[index].img
+            })
             this.selectFastMirror = response.data.result[0].systemtemplateid
           }
         })
