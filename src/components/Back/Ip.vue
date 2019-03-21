3884f8ae9f8ff14ebe48ff7796d6ff124db971b9
qrl <920149862@qq.com>
2019-03-21 17:08:15 +0800

    '局部刷新优化'

--- src/components/Back/Ip.vue
+++ src/components/Back/Ip.vue
@@ -929,6 +929,7 @@
       setData(response) {
         if (response.status == 200 && response.data.status == 1) {
           this.ipData = response.data.result.data
+          let publicipids = []
           if (!this.auth) {
             this.ipData.forEach(item => {
               item._disabled = true
@@ -937,16 +938,13 @@
           this.ipData.forEach(item => {
             if (item.status != 1 || item.status == 0) {
               item._disabled = true
-              if (item.publicipid) {
-                this.timingRefresh(item.publicipid)
-              } else {
-                setTimeout(() => {
-                  this.refresh()
-                }, 2000)
-              }
+              publicipids.push(item.publicipid)
             }
           })
           this.total = response.data.result.total
+          if(publicipids.length !==0) {
+            this.timingRefresh(publicipids + '')
+          }
           this.select.forEach(item => {
             this.ipData.forEach(ip => {
               if (item.id === ip.id) {
--- src/components/Back/newHost.vue
+++ src/components/Back/newHost.vue
@@ -1630,16 +1630,16 @@
           if (res.data.status == 1 && res.status == 200) {
             this.hostListData = res.data.result.data
             this.hostPages = res.data.result.total
+            let ids = []
             this.hostListData.forEach(host => {
               if (host.status == 2 || host.status == -2) {
                 host._disabled = true
-                if (host.id) {
-                  this.timingRefresh(host.id)
-                } else {
-                  this.getHostList()
-                }
+                ids.push(host.id)
               }
             })
+            if (ids.length !== 0) {
+              this.timingRefresh(ids + '')
+            }
             if (!this.auth) {
               this.hostListData.forEach(host => {
                 host._disabled = true
@@ -1840,10 +1840,10 @@
       hostDelete(val) {
         this.hostDelWay = val
         if (val === 1) {
-          if (this.hostSelection.length > 5) {
-            this.$Message.info('删除主机至多选择 5 项')
-            return
-          }
+          /*          if (this.hostSelection.length > 5) {
+                      this.$Message.info('删除主机至多选择 5 项')
+                      return
+                    }*/
           this.showModal.delHost = true
         } else {
           this.showModal.delHost = true
