c26cab62f79174a8965d9a8c702cc473d6c6eb51
eCheo <e1511404018@qq.com>
2019-03-29 11:25:52 +0800

    d

--- src/components/Back/GpuList.vue
+++ src/components/Back/GpuList.vue
@@ -1750,7 +1750,7 @@
 
         //创建镜像
       createMrrior(){
-        if(this.selectLength.length != 1){
+        if(this.selectLength.length > 1){
           this.$Message.info('一次只能选择一台主机镜像制作');
           return;
         }
