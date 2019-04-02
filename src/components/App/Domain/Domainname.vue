b4df61fbc15ca258d280d7132fa93db596bdeb1f
eCheo <e1511404018@qq.com>
2019-04-02 10:59:05 +0800

    d

--- src/components/App/Domain/Domainname.vue
+++ src/components/App/Domain/Domainname.vue
@@ -245,7 +245,7 @@
         sessionStorage.setItem('name', this.searchText)
         sessionStorage.setItem('suffix', JSON.stringify(this.suffixList))
         sessionStorage.setItem('suffixChange', JSON.stringify(this.getSuffix))
-        this.$router.push('DomainResult')
+        this.$router.push('/DomainResult')
       },
 
       showBtn(){
