2976ff66a9e13448a093d2d93cc48640812caa9d
qrl <920149862@qq.com>
2019-05-15 19:48:28 +0800

    '5.15部署'

--- src/App.vue
+++ src/App.vue
@@ -70,7 +70,7 @@
             <li @mouseenter="ME(1,$event)">
               <div class="menu-dropdown">
                 <div class="menu-dropdown-rel">
-                  <a href="https://zschj.xrcloud.net/overview" rel="nofollow"><span>控制台</span>
+                  <a href="https://www.xrcloud.net/overview" rel="nofollow"><span>控制台</span>
                   </a>
                 </div>
               </div>
@@ -85,7 +85,7 @@
             <li @mouseenter="ME(1,$event)">
               <div class="menu-dropdown">
                 <div class="menu-dropdown-rel">
-                  <a href="https://zschj.xrcloud.net/login" rel="nofollow"><span>登录</span>
+                  <a href="https://www.xrcloud.net/login" rel="nofollow"><span>登录</span>
                   </a>
                 </div>
               </div>
@@ -93,7 +93,7 @@
             <li @mouseenter="ME(1,$event)" style="background:#387Dff;width:100px;text-align:center;">
               <div class="menu-dropdown">
                 <div class="menu-dropdown-rels">
-                  <a href="https://zschj.xrcloud.net/register" rel="nofollow"><span>注册</span>
+                  <a href="https://www.xrcloud.net/register" rel="nofollow"><span>注册</span>
                   </a>
                 </div>
               </div>
@@ -231,7 +231,7 @@
               <a href="javascript:void(0)" target="_blank" style="cursor: auto">增值电信业务经营许可证B1-20180455</a>
             </li>
             <li>
-              <a href="https://zschj.xrcloud.net/about/" rel="nofollow">关于我们</a>
+              <a href="https://www.xrcloud.net/about/" rel="nofollow">关于我们</a>
             </li>
           </ul>
         </div>
