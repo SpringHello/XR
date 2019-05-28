652bc84cb4888de21c711f98b177fc4135b579ef
qrl <920149862@qq.com>
2019-05-28 17:47:36 +0800

    '调整'

--- src/components/App/buy/BHost.vue
+++ src/components/App/buy/BHost.vue
@@ -1513,8 +1513,9 @@
         }
         let len = val.length
         let reg = /[0-9]/
-        check(val.length)
-          function check(index,flag){
+        if(len>5){
+         let flag = check(len)
+          function check(index){
             let count = 0
             for(let i = index- 5; i < index;i++){
             let next = reg.test(val[i]) ? val[i] : val[i].charCodeAt()
@@ -1524,14 +1525,14 @@
              }
            }
             if(count > 4){
-              flag = true
-            }
-            if(flag){
               return true
-            } else{
-              return check(index - 1, false)
+            } else if(count < 5 && index > 5){
+              return check(index - 1)
+            } else if(index <= 5){
+              return false
             }
           }
+        }
         if(flag){
           this.passwordForm.secondDegree = false
         } else{
